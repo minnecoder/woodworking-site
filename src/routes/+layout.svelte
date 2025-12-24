15ba28f360dd003d8b167a61ab4ef51a2ab014fc
Sean Parkin <sdparkin@protonmail.com>
2025-12-23 20:09:48 -0600

    Added global css reset and font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
-
 	let { children } = $props();
 </script>
 
@@ -9,3 +8,12 @@
 </svelte:head>
 
 {@render children()}
+
+<style>
+	:global(body) {
+		padding: 0;
+		margin: 0;
+		font-family: 'Fig Tree', sans-serif;
+		font-size: 18px;
+	}
+</style>
