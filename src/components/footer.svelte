21ebfae4d73d308578f4b58aa612ac96f59fabdd
Sean Parkin <sdparkin@protonmail.com>
2025-12-23 20:09:13 -0600

    Created header and footer component

---
+++ src/components/footer.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { Icon } from 'svelte-icons-pack';
+	import { AiOutlineMail } from 'svelte-icons-pack/ai';
+	import { FaBrandsEtsy, FaBrandsFacebook, FaBrandsInstagram } from 'svelte-icons-pack/fa';
+	const currentYear = new Date().getFullYear();
+</script>
+
+<footer>
+	<div class="main">
+		<div class="links">
+			<a href="mailto:"><Icon src={AiOutlineMail} color="black" size="32" /></a>
+			<a href="/">
+				<Icon src={FaBrandsEtsy} color="black" size="32" />
+			</a>
+			<a href="/">
+				<Icon src={FaBrandsFacebook} color="black" size="32" />
+			</a>
+			<a href="/">
+				<Icon src={FaBrandsInstagram} color="black" size="32" />
+			</a>
+		</div>
+		&copy; {currentYear} Sean Parkin
+	</div>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: center;
+		padding: 2rem 0;
+	}
+	.links {
+		padding-bottom: 0.5rem;
+	}
+</style>
---
+++ src/components/header.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { Icon } from 'svelte-icons-pack';
+	import { FaBrandsEtsy, FaBrandsFacebook, FaBrandsInstagram } from 'svelte-icons-pack/fa';
+</script>
+
+<nav>
+	<a class="logo" href="/">Logo Goes Here!</a>
+	<ul class="links">
+		<li><a href="/">Home</a></li>
+		<li><a href="/about">About</a></li>
+		<li><a href="/work">Work</a></li>
+		<li><a href="/">Testimonials</a></li>
+		<li><a href="/">Contact</a></li>
+		<li><a href="/"><Icon src={FaBrandsFacebook} /></a></li>
+		<li><a href="/"><Icon src={FaBrandsEtsy} /></a></li>
+		<li><a href="/"><Icon src={FaBrandsInstagram} /></a></li>
+	</ul>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		margin: 0 10rem;
+	}
+	.logo {
+		padding-top: 0.75rem;
+		text-decoration: none;
+		color: #000;
+	}
+	.links {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+
+	.links li {
+		list-style: none;
+	}
+	.links a {
+		padding: 0rem 0.5rem;
+		text-decoration: none;
+		color: black;
+	}
+</style>
