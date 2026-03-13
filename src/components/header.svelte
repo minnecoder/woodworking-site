5a501f9d29b35d1c6b369781b27284fcb9fffcfd
Sean Parkin <sdparkin@protonmail.com>
2026-03-12 22:17:40 -0500

    Moved social links to different div

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -11,36 +11,61 @@
 		<li><a href="/work">Work</a></li>
 		<li><a href="/testimonials">Testimonials</a></li>
 		<li><a href="/contact">Contact</a></li>
-		<li><a href="/"><Icon src={FaBrandsFacebook} /></a></li>
-		<li><a href="/"><Icon src={FaBrandsEtsy} /></a></li>
-		<li><a href="/"><Icon src={FaBrandsInstagram} /></a></li>
 	</ul>
+	<div class="social-links">
+		<li class="social-icons"><a href="/"><Icon src={FaBrandsFacebook} /></a></li>
+		<li class="social-icons"><a href="/"><Icon src={FaBrandsEtsy} /></a></li>
+		<li class="social-icons"><a href="/"><Icon src={FaBrandsInstagram} /></a></li>
+		<li class="social-icons secret"><a href="/login">Secret</a></li>
+	</div>
 </nav>
 
 <style>
 	nav {
+		margin: 0 1rem;
+		width: 100%;
 		display: flex;
 		flex-direction: row;
-		justify-content: space-between;
-		margin: 0 10rem;
+		justify-content: space-around;
+		align-items: center;
 	}
 	.logo {
-		padding-top: 0.75rem;
+		padding-top: 0.2 5rem;
 		text-decoration: none;
 		color: #000;
 	}
-	.links {
+	.links,
+	.social-links {
 		display: flex;
 		flex-direction: row;
-		justify-content: space-evenly;
+		justify-content: space-around;
 	}
 
-	.links li {
+	.social-links {
+		margin-top: 0.75rem;
+	}
+
+	.links {
+		padding-top: 1rem;
+	}
+
+	.links li,
+	.social-links li {
 		list-style: none;
 	}
-	.links a {
+	.links a,
+	.social-links a {
 		padding: 0rem 0.5rem;
 		text-decoration: none;
 		color: black;
+	}
+
+	.social-icons {
+		margin-top: 0.25rem;
+	}
+	.secret a {
+		opacity: 0;
+		/* color: white; */
+		cursor: default;
 	}
 </style>
