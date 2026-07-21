342871f10fb66ed28b0eabc4c1ba7b3350645f44
Sean Parkin <sdparkin@protonmail.com>
2026-07-20 22:32:13 -0500

    Disabled work and testimonial links

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -9,8 +9,8 @@
 	<ul class="links">
 		<li><a href="/">Home</a></li>
 		<li><a href="/about">About</a></li>
-		<li><a href="/work">Work</a></li>
-		<li><a href="/testimonials">Testimonials</a></li>
+		<!-- <li><a href="/work">Work</a></li>
+		<li><a href="/testimonials">Testimonials</a></li> -->
 		<li><a href="/contact">Contact</a></li>
 	</ul>
 	<div class="social-links">
